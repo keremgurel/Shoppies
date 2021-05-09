0ccbd01e7b77c390ebc2f1f6dd2b9400d7a5399c
Kerem Gurel <keremgurell99@gmail.com>
2021-05-08 22:07:03 -0700

    fix: cleanup

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ function App() {
   const [results, setResults] = useState([]);                  // for results
   const [nominations, setNominations] = useState([]);          // for nominations
 
+  const updateSearchResults = (updatedSearchQuery) => {
+    // update local state to store the search query
+    setQuery(updatedSearchQuery);
+    // make an API call with the updatedSearchQuery
+    fetch(`https://www.omdbapi.com/?s=${updatedSearchQuery}&apikey=e524dabe`)
+      .then(res => res.json())
+      .then(res => console.log('Got the search results as: ', res));
+    // update local state to store the search results
+  };
 
   return (
     <div>
@@ -18,7 +27,9 @@ function App() {
         <h1>The Shoppies</h1>
         <img class="header-logo" src={ logo } alt="" />
       </div>
-      <SearchBar query={query} setQuery={setQuery} />
+      <SearchBar 
+        query={query}
+        setQuery={updateSearchResults} />
       <div className="row">
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
           <SearchResults query={query} />
