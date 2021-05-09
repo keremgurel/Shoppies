ba9ace325d2c0dbaff81bdd36dec197db8ecd26c
Kerem Gurel <keremgurell99@gmail.com>
2021-05-08 21:41:22 -0700

    general UI done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import Nominations from './Nominations';
 
 function App() {
+  const [query, setQuery] = useState('');                       // for query
+  const [results, setResults] = useState('');                   // for results
+  const [nominations, setNominations] = useState([]);           // for nominations
+
+
   return (
     <div>
       <h1>The Shoppies</h1>
-      <SearchBar />
+      <SearchBar query={query} queryChange={setQuery} />
       <div className="row">
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
           <SearchResults />
