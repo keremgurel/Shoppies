368dd664efd9b6c9126c4c9dc718fa057a14a969
Kerem Gurel <keremgurell99@gmail.com>
2021-05-08 21:21:00 -0700

    feat: title and search bar initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <div className="App">
       <h1>The Shoppies</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+export default function SearchBar({ query, setQuery }) {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="movieTitle" className="form-label">
+              Movie Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="movieTitle"
+              aria-describedby="movieTitle"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
